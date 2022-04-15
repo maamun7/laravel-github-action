8375fbc9112be75ab3af4a1bef64555cac6e9942
Mamun Ahmed <maamun7@gmail.com>
2022-04-16 00:37:42 +0600

    Update code

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -24,15 +24,15 @@ class TestController extends Controller
     public function function2(): bool
     {
         $use = 1;
-        do {
-            echo "The number is: $use <br>";
+        do
+        {
+            echo "The number ---------------------------------------------------------------------------------is: $use <br>";
             $use++;
-        } while ($use <= 5);
+        } while($use <= 5);
 
-        for ($use = 0; $use <= 100; $use += 10) {
+        for($use = 0; $use <= 100; $use += 10){
             echo "The number is: $use <br>";
         }
-
         return false;
     }
 }
