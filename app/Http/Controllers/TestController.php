f487d87934ae6f49623723049b6b6ecf49872476
Mamun Ahmed <maamun7@gmail.com>
2022-04-16 20:37:40 +0600

    Remove unncessary things

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -3,35 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Services\TestService;
 
 class TestController extends Controller
 {
-    /**
-     * @return bool
-     */
-    public function function1(): bool
-    {
-        if(-1 == true) {
-            return true;
-        }
+    public TestService $test;
 
-        return false;
+    public function __construct(TestService $testService)
+    {
+        $this->test = $testService;
     }
 
     /**
-     * @return bool
+     * @return void
      */
-    public function function2(): bool
+    public function test(): void
     {
-        $use = 1;do
-        {
-            echo "The number ---------------------------------------------------------------------------------is: $use <br>";
-            $use++;
-        } while($use <= 5);
-
-        for($use = 0; $use <= 100; $use += 10){
-            echo "The number is: $use <br>";
-        }
-        return false;
+        dd($this->test->getAll());
     }
 }
